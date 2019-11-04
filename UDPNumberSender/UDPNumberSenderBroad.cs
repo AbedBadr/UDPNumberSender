869841e8680bb4de2a8bf7bbc1e2590e77616950
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2019-11-04 01:21:38 +0100

    Added new class for broadcasting

---
+++ UDPNumberSender/UDPNumberSenderBroad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPNumberSender
+{
+    class UDPNumberSenderBroad
+    {
+        static void Main1(string[] args)
+        {
+            int number = 0;
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            UdpClient udpSender = new UdpClient();
+        }
+    }
+}
